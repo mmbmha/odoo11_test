0de5b22e094362bd2e238c0bfc1c1676d013825a
mmbmha <mmbmha@me.com>
2017-10-09 14:32:15 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Dall',
+    'version': '1.0',
+    'category': 'Test',
+    'summary': 'The offical app for Dall consulting',
+    'depends': ['web'],
+    'data': [
+        'data/actions.xml',
+        'data/plant_views.xml',
+        'data/order_views.xml',
+    ],
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
